4e3b193fc96271f04e801f5fa12d273d9db90a44
victortads <vgs8@discente.ifpe.edu.br>
2023-11-10 18:17:03 -0300

    novas funções

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
 const pupil = document.getElementsByClassName('pupil');
+const boca = document.getElementsByClassName('boca');
+const olhos = document.getElementById('eyes');
 
 document.onmousemove = (event) => {
     const x = event.clientX * 100 / window.innerWidth + '%'
     const y = event.clientY * 100 / window.innerHeight + '%'
 
     for (let i = 0; i < 2; i++) {
-        pupil[i].style.left = x
-        pupil[i].style.top = y
+        pupil[i].style.left = x;
+        pupil[i].style.top = y;
 
-        pupil[i].style.transform = `translate( -${x}, -${y})`
+        pupil[i].style.transform = `translate( -${x}, -${y})`;
     }
 }
+
+function randomColor() {
+    let x = parseInt(Math.random() * 255);
+    let y = parseInt(Math.random() * 255);
+    let z = parseInt(Math.random() * 255);
+    return `rgba(${x}, ${y}, ${z})`;
+}
+
+function addPupilListener() {
+    for (let a of pupil) {
+        a.addEventListener("click", alterPupilColor);
+    }
+}
+
+function addBocaListener() {
+    for (let a of boca) {
+        a.addEventListener("click", alterBoca);
+    }
+}
+
+function alterBoca(){
+    if (document.getElementById('imagem').classList.contains('displayOn')) {
+        document.getElementById('imagem').classList.remove('displayOn');
+        document.getElementById('imagem').classList.add('displayNone');
+        document.getElementById('imagem2').classList.remove('displayNone');
+        document.getElementById('imagem2').classList.add('displayOn');
+    } else {
+        document.getElementById('imagem2').classList.remove('displayOn');
+        document.getElementById('imagem2').classList.add('displayNone');
+        document.getElementById('imagem').classList.remove('displayNone');
+        document.getElementById('imagem').classList.add('displayOn');
+    }
+}
+
+function alterPupilColor() {
+    let color = randomColor();
+    for (let i in pupil) {
+        pupil[i].style.border = `8px solid ${color}`;
+    };
+}
+
+addPupilListener();
+addBocaListener();
